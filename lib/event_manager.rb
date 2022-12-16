01600e6a146a826f467fe392ba253f38a98479ca
CassieV <obicassievirtue@gmail.com>
2022-12-16 11:50:20 +0100

    Complete

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -15,6 +16,17 @@ def clean_phone(phone)
   else
     puts "N/A"
   end
+end
+
+def peak_hours(peak_time)
+  # puts peak_time
+  hour = Time.strptime(peak_time, '%m/%d/%y %k:%M').strftime('%k')
+  puts hour
+end
+
+def peak_days(peak_day)
+  day = Time.strptime(peak_day, '%m/%d/%y %k:%M').strftime('%A')
+  puts day
 end
 
 def legislators_by_zipcode(zip)
@@ -59,6 +71,8 @@ contents.each do |row|
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
   phone = clean_phone(row[:homephone])
+  peak_time = peak_hours(row[:regdate])
+  peak_day = peak_days(row[:regdate])
 
   form_letter = erb_template.result(binding)
 
